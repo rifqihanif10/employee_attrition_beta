b830dd52267006f5fe8ff69a7455c397a13c4b96
rifqihanif10 <61383576+rifqihanif10@users.noreply.github.com>
2022-11-29 07:13:59 +0700

    Update pos-ARMS-pub.py

--- pos-ARMS-pub.py
+++ pos-ARMS-pub.py
@@ -286,35 +286,6 @@ def halaman_utama():
         global data_multi
         halaman_multi()
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets.password.password:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password salah")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if check_password():
-    halaman_utama()
+halaman_utama()
     
 
